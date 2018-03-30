d591f22afe4e6223fd3614c12de24d7933a30dfe
Francois Ribalet <ribalet@uw.edu>
2018-03-30 11:51:07 -0700

    update calibration script

--- flow_rate_calibration/flow_rate_calibration.R
+++ flow_rate_calibration/flow_rate_calibration.R
@@ -1,8 +1,7 @@
 library(lmodel2)
 
-
 #Path to the Git repository
-setwd("~/Documents/DATA/Codes/seaflow-virtualcore")
+setwd("~/Documents/DATA/Codes/seaflow-sfl")
 
 fr <- read.csv("flow_rate_calibration/FLOW_RATEcalibration.csv")
 fr$fr <- 60*(fr$weight - fr$tare)/fr$time #calculate flow rate (ml/min)
@@ -12,6 +11,12 @@ fr$fr <- 60*(fr$weight - fr$tare)/fr$time #calculate flow rate (ml/min)
 reg <- lmodel2(fr ~ measured.pressure, data=log10(fr[,c("fr", "measured.pressure")]))
 df <- data.frame(expo=reg$regression.results$Slope[1],expo_97.5=reg$confidence.intervals[1,5],expo_2.5=reg$confidence.intervals[1,4],coeff=10^reg$regression.results$Intercept[1], coeff_97.5=10^reg$confidence.intervals[1,3],coeff_2.5=10^reg$confidence.intervals[1,2])
 
+
+psi <- 12
+FR <- 10^reg$regression.results$Intercept[1]*psi^reg$regression.results$Slope[1]
+FR_2.5 <- 10^reg$confidence.intervals[1,2]*psi^reg$confidence.intervals[1,4]
+FR_97.5 <- 10^reg$confidence.intervals[1,3]*psi^reg$confidence.intervals[1,5]
+error <- 100*mean(abs(c(1-FR_97.5/FR,1-FR_2.5/FR))) #calculate % error in FR determination
 
 png("flow_rate_calibration/FlowRate-calibration.png",width=12, height=12, unit='in', res=100)
 
@@ -23,7 +28,8 @@ par(mfrow=c(1,1), cex=1.4)
   abline(b=reg$confidence.intervals[1,4], a=reg$confidence.intervals[1,2], col='grey',lwd=2) # 2.5%
   abline(b=reg$confidence.intervals[1,5], a=reg$confidence.intervals[1,3], col='grey',lwd=2) # 97.5%
   legend("topleft", legend=bquote(paste("FR=",.(round(10^reg$regression.results$Intercept[1],3)),"(psi"^{.(round(reg$regression.results$Slope[1],3))},")")), bty='n',cex=2)
-  legend("bottomright", legend=bquote(paste("12 psi =",.(round(10^reg$regression.results$Intercept[1]*12^reg$regression.results$Slope[1],1)),
-                                                  "(",.(round(10^reg$confidence.intervals[1,2]*12^reg$confidence.intervals[1,4],1)),
-                                                  "-",.(round(10^reg$confidence.intervals[1,3]*12^reg$confidence.intervals[1,5],1)),")")), bty='n',cex=2)
+  legend("bottomright", bty='n',cex=2, legend=bquote(paste(.(round(psi))," psi =",.(round(FR,1)),"(+/- ",.(round(error,1)),"%) ml/min")))
+
 dev.off()
+
+write.csv(df,file=paste0("flow_rate_calibration/seaflow_FR-calibration.csv"), row.names=FALSE)
