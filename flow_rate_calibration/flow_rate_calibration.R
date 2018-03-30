01eed20ff84bf755e69a8cbd718cc20052689630
Francois Ribalet <ribalet@uw.edu>
2018-03-30 12:15:10 -0700

    add instrument serial number to FR calibration

--- flow_rate_calibration/flow_rate_calibration.R
+++ flow_rate_calibration/flow_rate_calibration.R
@@ -1,15 +1,14 @@
 library(lmodel2)
 
-#Path to the Git repository
+### Path to the Git repository
 setwd("~/Documents/DATA/Codes/seaflow-sfl")
 
 fr <- read.csv("flow_rate_calibration/FLOW_RATEcalibration.csv")
 fr$fr <- 60*(fr$weight - fr$tare)/fr$time #calculate flow rate (ml/min)
 
 
-# linear regression type II
+### linear regression type II
 reg <- lmodel2(fr ~ measured.pressure, data=log10(fr[,c("fr", "measured.pressure")]))
-df <- data.frame(expo=reg$regression.results$Slope[1],expo_97.5=reg$confidence.intervals[1,5],expo_2.5=reg$confidence.intervals[1,4],coeff=10^reg$regression.results$Intercept[1], coeff_97.5=10^reg$confidence.intervals[1,3],coeff_2.5=10^reg$confidence.intervals[1,2])
 
 
 psi <- 12
@@ -31,5 +30,12 @@ par(mfrow=c(1,1), cex=1.4)
   legend("bottomright", bty='n',cex=2, legend=bquote(paste(.(round(psi))," psi =",.(round(FR,1)),"(+/- ",.(round(error,1)),"%) ml/min")))
 
 dev.off()
+
+
+### Save equation for conversion from stream pressure (psi) to flow rate (ml/min)
+df1 <- data.frame(inst=740, expo=reg$regression.results$Slope[1],expo_97.5=reg$confidence.intervals[1,5],expo_2.5=reg$confidence.intervals[1,4],coeff=10^reg$regression.results$Intercept[1], coeff_97.5=10^reg$confidence.intervals[1,3],coeff_2.5=10^reg$confidence.intervals[1,2])
+# while we wait to do the calibration for #751, we are assuming identical calibration
+df2 <- data.frame(inst=751, expo=reg$regression.results$Slope[1],expo_97.5=reg$confidence.intervals[1,5],expo_2.5=reg$confidence.intervals[1,4],coeff=10^reg$regression.results$Intercept[1], coeff_97.5=10^reg$confidence.intervals[1,3],coeff_2.5=10^reg$confidence.intervals[1,2])
+df <- rbind(df1, df2
 
 write.csv(df,file=paste0("flow_rate_calibration/seaflow_FR-calibration.csv"), row.names=FALSE)
